fe10b5fa22594305174061fea5802cfd7ec8670b
Miguel Mendoza <miguel12321@gmail.com>
2020-06-14 14:58:32 -0400

    deploy

--- src/Pages/HomePage.js
+++ src/Pages/HomePage.js
@@ -147,7 +147,7 @@ export default function Album() {
 				</div>
 				<Container className={classes.cardGrid} maxWidth="md">
 					{/* End hero unit */}
-					<Grid container spacing={4}>
+					<Grid container spacing={4}> 
 						{cards.map((card) => (
 							<Grid item key={card} xs={12} sm={6} md={4}>
 								<Card className={classes.card}>
